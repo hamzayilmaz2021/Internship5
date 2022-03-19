969d4a164dae4850da541d83fba725645cd64cd6
hamza <davut.erarslan@gmail.com>
2022-03-19 22:39:19 +0300

    v001

---
+++ src/Deneme.java
@@ -0,0 +1,13 @@
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Hamza - 20:29 - Master Branch");
+		System.out.println("Hamza - 20:33 - Hamza Branch");
+	}
+
+}
+
+
+// git branch : branchleri kontrol eder.
+// git branch hamza : hamza adýnda branch olusturur.
+// git checkout hamza : hamza branchine gecis yapar
