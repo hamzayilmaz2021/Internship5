06b707f747b5f4849173b875207456bc41ff4014
Rose <rs8953363@gmail.com>
2022-03-19 15:23:45 -0500

    Gul

---
+++ Deneme.java
@@ -0,0 +1,18 @@
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Hamza - 20:29 - Master Branch");
+		System.out.println("Hamza - 20:33 - Hamza Branch");
+		System.out.println("Hamza - 20:40 - Hamza Branch");
+		System.out.println("Hamza - 20:45 - Hamza Branch");
+		System.out.println("Gul   - 21:01 - Gul   Branch");
+		
+	}
+
+}
+
+
+// git branch : branchleri kontrol eder.
+// git branch hamza : hamza adýnda branch olusturur.
+// git checkout hamza : hamza branchine gecis yapar
+// git merge master : master branchini hamza branchine ekliyorum
